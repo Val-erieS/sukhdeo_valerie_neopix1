dc1b47995616ddae1e3a0cee072799e5b6843164
Val-erieS <72756972+Val-erieS@users.noreply.github.com>
2020-11-09 13:01:06 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 light.set_brightness(10)
-light.set_pixel_color(0, light.rgb(255,0,0))
-light.set_pixel_color(1, light.rgb(0,0,255))
-light.set_pixel_color(2, light.rgb(0,255,0))
-light.set_pixel_color(3, light.rgb(255,255,0))
-light.set_pixel_color(4, light.rgb(255,0,255))
-light.set_pixel_color(5, light.rgb(0,255,255))
-light.set_pixel_color(6, light.rgb(39,109,194))
-light.set_pixel_color(7, light.rgb(205,103,153))
-light.set_pixel_color(8, light.rgb(234,32,94))
-light.set_pixel_color(9, light.rgb(165,7,62))
 while True:
-    light.clear() and pause(3000)
-    light.clear() and pause(2000)
-
-
-
-
+    light.set_pixel_color(0, light.rgb(255,0,0))
+    light.set_pixel_color(1, light.rgb(0,0,255))
+    light.set_pixel_color(2, light.rgb(0,255,0))
+    light.set_pixel_color(3, light.rgb(255,255,0))
+    light.set_pixel_color(4, light.rgb(255,0,255))
+    light.set_pixel_color(5, light.rgb(0,255,255))
+    light.set_pixel_color(6, light.rgb(39,109,194))
+    light.set_pixel_color(7, light.rgb(205,103,153))
+    light.set_pixel_color(8, light.rgb(234,32,94))
+    light.set_pixel_color(9, light.rgb(165,7,62))
+    pause(2000)
+    light.clear()
+    pause(2000)
